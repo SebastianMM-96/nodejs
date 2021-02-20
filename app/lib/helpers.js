1134b956bad794695ad593ae31d44404c6293dcc
Sebastian Marroquin <sebasmarro10@gmail.com>
2021-02-19 22:28:28 -0600

    Twilio SMS :rocket:

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -5,6 +5,8 @@
  /** Dependencies */
 var crypto = require('crypto');
 var config = require('./config');
+var queryString = require('querystring');
+var https = require('https');
 
 /** Container for helpers */
 var helpers = {};
@@ -50,6 +52,56 @@ helpers.createRandomString = (strLength)=>{
         return str;
     }else{
         return false;
+    }
+}
+
+/** Send a SMS message */
+helpers.sendTwilioSms = (phone, msg, callback)=>{
+    //validate the params
+    phone = typeof(phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false;
+    msg = typeof(msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg.trim() : false;
+    if(phone && msg){
+        //Config the request payload
+        var payload = {
+            'From' : config.twilio.fromPhone,
+            'To' : '+1' + phone,
+            'Body' : msg
+        };
+        //stringify the payload
+        var stringPayload = queryString.stringify(payload);
+        var requestDetails = {
+            'protocol' : 'https:',
+            'hostname' : 'api.twilio.com',
+            'method' : 'POST',
+            'path' : '/2010-04-01/Accounts/' + config.twilio.accountsId + '/Messages.json',
+            'auth' : config.twilio.accountsId + ':' + config.twilio.authToken,
+            'headers' : {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Content-Length' : Buffer.byteLength(stringPayload)
+            }
+        };
+        var req = https.request(requestDetails, (res)=>{
+            var status = res.statusCode;
+            if(status == 200 || status == 201){
+                callback(false);
+            }else{
+                callback('Status code was: ' + status);
+            }
+        });
+        
+        // Bind the error event
+        req.on('error', (e)=>{
+            callback(e);
+        });
+
+        // Add the payload
+        req.write(stringPayload);
+
+        // End the request
+        req.end();
+
+    }else{
+        callback('Given parameters were missing');
     }
 }
 
